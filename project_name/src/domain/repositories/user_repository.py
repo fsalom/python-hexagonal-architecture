e178067ad8a5ae068440e744f6d25db6555c1ec7
Fernando Salom <fer@rudo.es>
2024-07-03 22:48:32 +0200

    Remove old version

--- project_name/src/domain/repositories/user_repository.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from project_name.src.domain.entities.user import User
-
-
-class UserRepository(ABC):
-    @abstractmethod
-    def save(self, user: User):
-        pass
