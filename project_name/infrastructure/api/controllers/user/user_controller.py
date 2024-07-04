3b39e79342951bc5f9c63811ad63915a2b49032b
Fernando Salom <fer@rudo.es>
2024-07-04 10:21:42 +0200

    Add api controller

---
+++ project_name/infrastructure/api/controllers/user/user_controller.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from fastapi import APIRouter
+
+from project_name.domain.services.user import UserServices
+from project_name.infrastructure.adapters.user.user_in_memory import InMemoryUserAdapter
+from project_name.infrastructure.api.serializers.user.user_serializer import UserSerializer
+
+router = APIRouter()
+
+user_adapter = InMemoryUserAdapter()
+service = UserServices(user_adapter)
+
+
+@router.post("/users/", response_model=UserSerializer)
+def create_user(user_data: UserSerializer):
+    service.create(user=user_data)
+
+
+@router.get("/users/", response_model=List[UserSerializer])
+def list_users():
+    users = service.get()
+    return [UserSerializer.from_orm(user) for user in users]
